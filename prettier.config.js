107267d255de1d6e16323dd1ac882634b8788b21
RED143 <b.zabalotny@gmail.com>
2021-05-11 16:44:38 +0300

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  semi: false,
+};
