3db824db04fb656de232bab8586f80d848edf07f
RED143 <b.zabalotny@gmail.com>
2021-05-11 16:12:18 +0300

    Add typescript support

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Weather</title>
+      </Head>
+      <p>weather app</p>
+    </>
+  )
+}
